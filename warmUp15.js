c38900a88e116e2f1c33854f5f3acc2949e2e0c4
Abdo Alabed <henry_3030@icloud.com>
2019-11-05 19:26:56 +0100

    done

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,35 @@
 //
 //
 // Note : you can`t use any built-in functions .
+function reverseStr(str){
+	var arr = [];
+	var s = '';
+	
+	for (var i = 0; i < str.length; i++) {
+         
+		if(str[i]!==' '){
+		
+			s=s+str[i]
+		}else{
+			
+			arr.push(s);
+			s = '';
+		}
+	}
+	s= '';
+
+	for (var i = str.length-1; i > 0; i--) {
+		if(str[i]!==' '){
+			s=s+str[i]
+		}else{
+			arr.push(s);
+			s = '';
+			i=0;
+		}
+	} var st = '';
+	 for (var i = arr.length-1; i >= 0; i--) {
+	 	st = st + arr[i] + ' ';
+	 }
+	 return st;
+	
+}
