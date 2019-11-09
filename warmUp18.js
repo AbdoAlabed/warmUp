809202d2708031310259a61d02a7ffe0f4b879d1
Abdo Alabed <henry_3030@icloud.com>
2019-11-09 09:58:37 +0100

    done

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,30 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+function example(str){
+	var result = '1';
+	var arr = [];
+	arr[0] = str[0];
+	var c = 1;
+    var sum = 1;
+    var sum2 = 1;
+    var stop = str.length;
+   for (var i = 0; i < arr.length; i++) {
+   	 
+   	 	if(str[c] !== arr[i] ){
+            result = result + sum;
+   	 		 arr[c] = str[c];
+   	 		 c++;
+   	 	     
+   	 } else if(str[c] === arr[i]) {
+   	 	sum2++;
+   	 	result = result +sum2;
+   	 	arr[c] = str[c];
+   	 	c++;
+   	 }
+   	 if(stop === 0){
+   	 	break;
+   	 }
+      stop --;
+   }return result;
+}
